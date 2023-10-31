816c460d3f5979bd4c0921bb026138709be17e5f
Maya <maya.atwan.2000@gmail.com>
2023-10-31 22:47:16 +0200

    adding new folders

---
+++ lib/screens/Login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatelessWidget {
+  final string userName;
+  final string password;
+
+  const LogIn({
+    required this.userName,
+    required this.password,
+  });
+}
