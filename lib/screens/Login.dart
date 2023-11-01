7e9b4a9062a432697ba645165b980c0722ad8a1d
maryqubty <qubtymary1@gmail.com>
2023-11-01 14:40:14 +0200

    adding tabbar

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 class LogIn extends StatelessWidget {
-  final string userName;
-  final string password;
+  final String userName;
+  final String password;
 
   const LogIn({
     required this.userName,
     required this.password,
   });
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
 }
