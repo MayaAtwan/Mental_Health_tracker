64e145011174f0a5ab769948f60bad58fe8410b2
maryqubty <qubtymary1@gmail.com>
2023-11-01 15:19:39 +0200

    updated upperBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,56 @@
-import "package:flutter/material.dart";
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-runApp(upperBar()); //MaterialApp
+  runApp(upperBar());
 }
 
 MaterialApp upperBar() {
-return MaterialApp(
-	home: Scaffold(
-	appBar: AppBar(
-		title: const Text("PeacePath"),
-		actions: <Widget>[
-		IconButton(
-			icon: const Icon(Icons.comment),
-			tooltip: 'Comment Icon',
-			onPressed: () {},
-		), //IconButton
-		IconButton(
-			icon: const Icon(Icons.settings),
-			tooltip: 'Setting Icon',
-			onPressed: () {},
-		), //IconButton
-		], //<Widget>[]
-		backgroundColor: const Color(0xFF5B8BDF),
-		elevation: 50.0,
-		leading: IconButton(
-		icon: const Icon(Icons.menu),
-		tooltip: 'Menu Icon',
-		onPressed: () {},
-		),
-		//systemOverlayStyle: SystemUiOverlayStyle.light,
-	), //AppBar
-	), //Scaffold
-	debugShowCheckedModeBanner: false, //Removing Debug Banner
-);
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("PeacePath"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.comment),
+            tooltip: 'Comment Icon',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Setting Icon',
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: const Color(0xFF5B8BDF),
+        elevation: 50.0,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          tooltip: 'Menu Icon',
+          onPressed: () {},
+        ),
+        flexibleSpace: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    // Handle the first button press
+                  },
+                  child: Text("Button 1"), 
+                  backgroundColor: const Color(0xFF5B8BDF),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Handle the second button press
+                  },
+                  child: Text("Button 2"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
